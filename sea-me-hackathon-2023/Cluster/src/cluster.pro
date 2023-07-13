4bc8bf7f940243832852e8bd003b58034cb4cfb3
SeoHyun Kim <tjgus5569@naver.com>
2023-07-13 06:31:20 +0000

    Add files via upload

--- sea-me-hackathon-2023/Cluster/src/cluster.pro
+++ sea-me-hackathon-2023/Cluster/src/cluster.pro
@@ -4,6 +4,7 @@ QT += multimedia
 QT += network
 QT += webengine
 QT += webenginewidgets
+QT += widgets
 
 # The following define makes your compiler emit warnings if you use
 # any feature of Qt which as been marked deprecated (the exact warnings
--- sea-me-hackathon-2023/Cluster/src/main.cpp
+++ sea-me-hackathon-2023/Cluster/src/main.cpp
@@ -1,11 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QApplication>
-#include <QQmlApplicationEngine>
-#include <QWebEngineView>
-#include <QWebChannel>
 #include <QQmlContext>
-#include "SpeedReceiver.h" // Include the new header
+#include "SpeedReceiver.h"
 #include "ButtonsReceiver.h"
 #include "RPMReceiver.h"
 #include <qqml.h>
@@ -15,9 +11,13 @@
 #include <thread>
 #include <CommonAPI/CommonAPI.hpp>
 #include "ClusterStubImpl.hpp"
-
-
 #include "WeatherAPI.h"
+
+// #include <QWebEngineSettings>
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QWebEngineView>
+#include <QWebChannel>
 
 using namespace std;
 Q_DECLARE_METATYPE(std::string)
@@ -27,8 +27,7 @@ int main(int argc, char *argv[])
     qRegisterMetaType<std::string>();
 
     std::shared_ptr<CommonAPI::Runtime> runtime = CommonAPI::Runtime::get();
-    std::shared_ptr<ClusterStubImpl> myService =
-        std::make_shared<ClusterStubImpl>();
+    std::shared_ptr<ClusterStubImpl> myService = std::make_shared<ClusterStubImpl>();
     runtime->registerService("local", "cluster_service", myService);
     std::cout << "Successfully Registered Service!" << std::endl;
 
@@ -46,9 +45,9 @@ int main(int argc, char *argv[])
     engine.rootContext()->setContextProperty("rpmReceiver", &rpmStorage);
     engine.rootContext()->setContextProperty("weatherAPI", &weatherAPIStorage);
 
-    QObject::connect(&(*myService), &ClusterStubImpl::signalSpeed, &speedStorage, &SpeedReceiver::receiveSpeed); // Connect the instances
-    QObject::connect(&(*myService), &ClusterStubImpl::signalButtons, &buttonStorage, &ButtonsReceiver::receiveButtons); // Connect the instances
-    QObject::connect(&(*myService), &ClusterStubImpl::signalRPM, &rpmStorage, &RPMReceiver::receiveRPM); // Connect the instances
+    QObject::connect(&(*myService), &ClusterStubImpl::signalSpeed, &speedStorage, &SpeedReceiver::receiveSpeed);
+    QObject::connect(&(*myService), &ClusterStubImpl::signalButtons, &buttonStorage, &ButtonsReceiver::receiveButtons);
+    QObject::connect(&(*myService), &ClusterStubImpl::signalRPM, &rpmStorage, &RPMReceiver::receiveRPM);
 
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
@@ -57,11 +56,20 @@ int main(int argc, char *argv[])
     QWebEngineView view;
     QWebChannel channel;
     view.page()->setWebChannel(&channel);
-    //channel.registerObject(QStringLiteral("backend"), &backend);
-    view.load(QUrl::fromLocalFile("/home/sea/sea-me-hackathon-2023/Cluster/src/map.html"));  // Replace with the path to your HTML file
+    view.load(QUrl::fromLocalFile("/home/sea/sea-me-hackathon-2023/Cluster/src/map.html"));
 
-    // Show the view
-    view.show();
+    // Check fuel condition and load the map if fuel is below 20%
+    int fuelLevel = 25; // Example fuel level, you can replace it with your actual fuel level
+    if (fuelLevel < 20) {
+        view.show();
+    }
+
+    // QWebEngineView view;
+    // QWebChannel channel;
+    // view.page()->setWebChannel(&channel);
+    // view.load(QUrl::fromLocalFile("/home/sea/sea-me-hackathon-2023/Cluster/src/map.html"));
+    // view.show();
+    
 
     return app.exec();
 }
